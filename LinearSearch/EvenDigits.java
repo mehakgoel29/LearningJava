babaee5b22c49cb7d97cec69b42d266b4139a647
Mehak Goel <mhkgoel29@gmail.com>
2024-08-04 11:07:19 +0530

    improvement

--- LinearSearch/EvenDigits.java
+++ LinearSearch/EvenDigits.java
@@ -4,23 +4,23 @@ public class EvenDigits {
         System.out.println(countDigits(arr));
     }
     static int countDigits(int[] arr){
-        int ans=0;
+        int EvenCount=0;
         for (int i = 0; i < arr.length; i++) {
-            int count=0;
+            int DigitCount=0;
             int num=arr[i];
             if(num==0){
-               count=1;
+                DigitCount=1;
             }
             else{
-                while(num>0){
-                    count++;
+                while(num>0){    // we are counting the number of digits in a number -> 234 ...it has 3 digits
+                    DigitCount++;
                     num=num/10;
                 }
-                if(count%2==0){
-                    ans++;
+                if(DigitCount%2==0){ // now here we are cheacking whether the number of digits are even in number or not 
+                    EvenCount++;
                 }
             }
         }
-        return ans;
+        return EvenCount;
     } 
 }
