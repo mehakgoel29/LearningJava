9394a559fe3e795fe863873c531cff3348db264f
Mehak Goel <mhkgoel29@gmail.com>
2024-08-05 20:43:02 +0530

    rotation count

--- BinarySearch/RotationCount.java
+++ BinarySearch/RotationCount.java
@@ -1,10 +1,78 @@
 public class RotationCount {
     public static void main(String[] args) {
-        int[] arr = {4, 1, 2,3};
-        rotationCount(arr);
+        int[] arr = {3,4,1,2};
+        System.out.println(countRotations(arr));
     }
 
-    public static void rotationCount(int[] nums) {
+    private static int countRotations(int[] arr) {
+        int pivot = findPivot(arr);
+        return pivot + 1;
+    }
 
-}
+    // use this for non duplicates
+    static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            /*Case 1 and Case 2: 
+            These directly identify the pivot point by checking if the current element or its adjacent elements form a pivot.
+            Case 3 and Case 4: 
+            These help in narrowing down the search space by moving the search boundaries (start and end).
+             */
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+            if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    // use this when arr contains duplicates
+    static int findPivotWithDuplicates(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+
+            // if elements at middle, start, end are equal then just skip the duplicates
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                // skip the duplicates
+                // NOTE: what if these elements at start and end were the pivot??
+                // check if start is pivot
+                if (arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+
+                // check whether end is pivot
+                if (arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted, so pivot should be in right
+            else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
