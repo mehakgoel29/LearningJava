14fd657bfe7043105e1b147428a82eb96d7d4d45
Mehak Goel <mhkgoel29@gmail.com>
2024-08-05 20:07:15 +0530

    improvement and revision

--- BinarySearch/RotationCount.java
+++ BinarySearch/RotationCount.java
@@ -1,3 +1,10 @@
 public class RotationCount {
-    
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 2,3};
+        rotationCount(arr);
+    }
+
+    public static void rotationCount(int[] nums) {
+
+}
 }
--- String/CountAndSay.java
+++ String/CountAndSay.java
@@ -1,32 +1,37 @@
 public class CountAndSay {
-    public static String countAndSay(int n) {
-        if(n==1){
-            return "1";
-        }
-        String currentTerm = "1";
-        for (int i = 2; i <= n; i++) {
-            String nextTerm = "";
-            int count = 1;
-            char digit = currentTerm.charAt(0);
-            for (int j = 1; j < currentTerm.length(); j++) {
-                if (currentTerm.charAt(j) == digit) {
-                    count++;
-                } else {
-                    nextTerm += count + "" + digit;
-                    digit = currentTerm.charAt(j);
-                    count = 1;
-                }
-            }
-            nextTerm += count + "" + digit;
-            currentTerm = nextTerm;
-        }
-        return currentTerm;
-    }
-    public static void main(String[] args) {
-        int n = 10;
-        for (int i = 1; i <=n; i++) {
-            String result = countAndSay(i);
-            System.out.println("The " + i + "th term is: " + result);
-        }
-    }
+
+	public static void main(String[] args) {
+		int n = 10;
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Term " + i + ": " + countandsay(i));
+		}
+
+	}
+	static String countandsay(int n) {
+		if (n == 1) {
+			return "1";
+		}
+		String prevterm=countandsay(n-1);
+		int count = 1;
+		String ans = "";
+		int i;
+		for ( i = 0; i < prevterm.length() - 1; i++) {
+			if (prevterm.charAt(i) == prevterm.charAt(i + 1)) {
+				count++;
+
+			} else {
+				ans += count;
+				ans += prevterm.charAt(i);
+				count=1;
+
+			}
+		}
+		ans += count;
+		ans += prevterm.charAt(i);
+	
+
+		return ans;
+
+	}
+
 }
