1b83ff5fdf05a77d7ba1c927c252404f95028c24
Mehak Goel <mhkgoel29@gmail.com>
2024-09-05 20:17:12 +0530

    Deletion in linked list

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,15 +1,28 @@
+import com.sun.security.jgss.GSSUtil;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        System.out.println("Linked List");
         SinglyLL list = new SinglyLL();
         // Add elements in front
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(7);
         list.insertFirst(29);
-
+        list.insertLast(38);
+        list.insertAtPos(6,3);
         // Display linked list
         list.display();
+        System.out.println("size :"+list.size);
+        System.out.println("element deleted: "+list.deleteFirst());
+        list.display();
+        System.out.println("Size after deletion: "+list.size);
+        System.out.println("list after last element deletion:"+list.deleteLast());
+        list.display();
+        System.out.println("list after deletion of element at a specific index:"+list.deleteAtPos(3));
+        list.display();
+
     }
 }
+
--- LinkedList/src/SinglyLL.java
+++ LinkedList/src/SinglyLL.java
@@ -2,12 +2,12 @@ public class SinglyLL {
 
         private Node head;
         private Node tail;
-        private int size;
+        public int size;
 
         public SinglyLL() {
             this.size = 0;
         }
-
+        //INSERTION
         public void insertFirst(int val) {
             Node newNode = new Node(val);
             newNode.next = head;
@@ -19,6 +19,79 @@ public class SinglyLL {
             size++;
         }
 
+        public void insertLast(int val){
+            Node newNode= new Node(val);
+            tail.next=newNode;
+            tail=newNode;
+            size++;
+
+        }
+
+        public void insertAtPos(int val ,int pos){
+            if(pos==0){
+               insertFirst(val);
+               return;
+            }
+            if(pos==size) {
+                insertLast(val);
+                return;
+            }
+            Node temp= head;
+            for (int i = 0; i <pos-1; i++) {
+                temp=temp.next;
+            }
+            Node newnode=new Node(val,temp.next);
+            temp.next=newnode;
+            size++;
+
+        }
+
+        //DELETION
+        public int deleteFirst(){
+            int val=head.val;
+            head=head.next;
+            if(head==null){
+                tail=null;
+            }
+            size--;
+            return val;
+        }
+
+        public Node get(int pos){
+            //pos is size-2 for the deletion of last element
+            Node temp= head;
+            for (int i = 0; i < pos; i++) {
+                temp=temp.next;
+            }
+            return temp;
+        }
+        public int deleteLast(){
+            int val = tail.val;
+            if(size<=1){
+                deleteFirst();
+            }
+            Node pos=get(size-2);
+            tail=pos;
+            tail.next=null;
+            size--;
+            return val;
+        }
+        public int deleteAtPos(int pos){
+           if(pos==0){
+               deleteFirst();
+           }
+           if(pos==size-1){
+               deleteLast();
+           }
+           Node prev=get(pos-1);
+           int val=prev.next.val;
+           prev.next=prev.next.next;
+
+           size--;
+           return val;
+
+        }
+        //DISPLAY
         public void display() {
             Node temp = head;
             while (temp != null) {
@@ -43,3 +116,4 @@ public class SinglyLL {
         }
 
 }
+
